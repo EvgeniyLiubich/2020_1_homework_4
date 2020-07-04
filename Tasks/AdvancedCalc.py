8fb6bb55ae847b9c74d3a116d9f9f95dfadce3a1
evgeniy_liubich <lyubitch89@yandex.by>
2020-07-04 17:20:21 +0300

    __name__

--- Tasks/AdvancedCalc.py
+++ Tasks/AdvancedCalc.py
@@ -49,9 +49,9 @@ Advanced calculator. Author: [Student Name], Version: 1.0.0
 
 
 class MyClass:
-
+    __name__ = 'Advanced calculator. Author: Liubich Evgeni, Version: 1.0.0'
+    print(__name__)
     def advanced_calc(self, formula_string):
-        print('Advanced calculator. Author: Liubich Evgeni, Version: 1.0.0')
         OPERATORS = {'+': (1, lambda x, y: x + y), '-': (1, lambda x, y: x - y),
                      '*': (2, lambda x, y: x * y), '/': (2, lambda x, y: x / y)}
         def parse(formula_string):
